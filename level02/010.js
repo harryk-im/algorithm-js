4b9413cb617d6f44217b12e459fda93d8d345203
harryk-im <hanyugeon@naver.com>
2026-04-03 19:16:36 +0900

    프로그래머스: 올바른 괄호 (LV2) 배열 활용한 스택 복습

--- level02/010.js
+++ level02/010.js
@@ -53,3 +53,59 @@ function solution(s) {
 
 //   return true;
 // }
+
+/**
+ * 26.04.03
+ * 스택을 활용한 풀이
+ * 1. 피드백 대로 빠르게 구현
+ * 2. 효율성에서 아쉬운 부분이 생겼음
+ * 3. ealry return을 활용한 예외처리로 효율성 개선
+ */
+function solution(s) {
+  const stack = [];
+  if (s[0] === ")" && stack.length === 0) return false;
+  if (s.length % 2 !== 0) return false;
+
+  for (const item of s) {
+    if (item === "(") {
+      stack.push("(")
+      continue;
+    } else {
+      const top = stack.pop()
+      if (!top) return false;
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
+
+/**
+ * 26.04.03
+ * 스택을 활용한 풀이
+ * 1. 피드백 대로 빠르게 구현
+ * 2. 효율성에서 아쉬운 부분이 생겼음
+ * 3. ealry return을 활용한 예외처리로 효율성 개선
+ */
+function solution(s) {
+  // 첫 item이 ')' 이면 return false;
+  if (s[0] === ")") return false;
+  // 길이가 홀수이면 return false;
+  if (s.length % 2 !== 0) return false;
+
+  const stack = [];
+
+  for (let idx = 0; idx < s.length; idx += 1) {
+    // idx가 절반이 넘어갔는데 stack의 길이가 절반보다 크면 return false;
+    if (idx > (s.length / 2) && stack.length > (s.length / 2)) return false;
+
+    if (s[idx] === "(") {
+      stack.push("(")
+      continue;
+    } else {
+      const top = stack.pop()
+      if (!top) return false;
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
